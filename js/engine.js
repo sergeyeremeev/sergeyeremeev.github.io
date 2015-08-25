8306e6801a8ace7639298d60d816922db103a0c0
sergeye <13.eremeev.sv@gmail.com>
2015-08-25 13:45:42 +0300

    final version of my arcade game clone project 3 submission

--- js/engine.js
+++ js/engine.js
@@ -25,8 +25,8 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    canvas.width = 606;
+    canvas.height = 707;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -109,14 +109,16 @@ var Engine = (function(global) {
          */
         var rowImages = [
                 'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/stone-block.png',   // Row 1 of 4 of stone
+                'images/stone-block.png',   // Row 2 of 4 of stone
+                'images/stone-block.png',   // Row 3 of 4 of stone
+                'images/stone-block.png',   // Row 4 of 4 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
-            numRows = 6,
-            numCols = 5,
+            // increased number of rows and cols from the initial version of the engine.js
+            numRows = 7,
+            numCols = 7,
             row, col;
 
         /* Loop through the number of rows and columns we've defined above
@@ -152,6 +154,12 @@ var Engine = (function(global) {
             enemy.render();
         });
 
+        // render obstacles before player, so they don't cover character's head
+        // and after bugs, so they appear to be crawling underneath the rocks
+        obstacles.forEach(function(obstacle) {
+            obstacle.render();
+        });
+
         player.render();
     }
 
@@ -172,7 +180,12 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Rock.png'
     ]);
     Resources.onReady(init);
 
