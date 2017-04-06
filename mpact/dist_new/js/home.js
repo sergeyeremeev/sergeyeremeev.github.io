17fea530edf08527316e6b8ec55793cbf30fa658
Sergey Eremeev <13.eremeev.sv@gmail.com>
2017-04-06 11:50:16 +0300

    updated version

--- mpact/dist_new/js/home.js
+++ mpact/dist_new/js/home.js
@@ -46,7 +46,7 @@
 
             init: function () {
                 // set each screen height to window height
-                mpact.setScreenSizes();
+                // mpact.setScreenSizes();
 
                 // scrolling buttons
                 mpact.settings.btnFeatures.on('click', mpact.homeSlideRight);
