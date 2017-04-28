c2f9c43d6011bfd85e7da2b6e957a8c9aa4a9353
Sergey Eremeev <13.eremeev.sv@gmail.com>
2017-04-29 01:04:26 +0300

    improved text animations, first car animations

--- hulq/assets/bundle.js
+++ hulq/assets/bundle.js
@@ -63,102 +63,11 @@
 /******/ 	__webpack_require__.p = "assets";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-var jQuery = __webpack_require__(3);
-
-(function ($) {
-
-    var hulq_landing = {
-
-        elements: {
-            contentBlocks: $('.landing-block'),
-            contentBlockFirst: $('.landing-block--top'),
-            contentBlockLast: $('.landing-block--bottom'),
-            actionButtonMain: $('.button--landing-main'),
-            carCentral: $('.car-center')
-        },
-
-        init: function () {
-            var that = this;
-
-            this.showInitialText();
-
-            $(window).on('scroll.animate-text', that.scrolling.bind(this));
-            $(window).on('scroll', that.toggleActionButtonView.bind(this));
-        },
-
-        showInitialText: function () {
-            this.elements.contentBlockFirst.add(this.elements.actionButtonMain).addClass('appear');
-        },
-
-        scrolling: function () {
-            var documentHeight = $(document).height(),
-                scrolledHeight = $(document).scrollTop(),
-                windowHeight = $(window).height(),
-                combinedHeight = scrolledHeight + windowHeight;
-
-            this.elements.contentBlocks.not('.landing-block--top').each(function () {
-                var $this = $(this);
-                if (scrolledHeight + windowHeight + 100 >= $this.offset().top) {
-                    $this.addClass('appear');
-                }
-            });
-
-            if (!this.elements.contentBlocks.not('.visible').length) {
-                console.log('unboun');
-                $(window).off('scroll.animate-text');
-            }
-        },
-
-        toggleActionButtonView: function () {
-            var scrolledHeight = $(document).scrollTop(),
-                windowHeight = $(window).height();
-
-            if (scrolledHeight + windowHeight >= this.elements.contentBlockLast.offset().top) {
-                this.elements.actionButtonMain.addClass('disappear');
-            } else {
-                this.elements.actionButtonMain.removeClass('disappear');
-            }
-
-        }
-    };
-
-    $(function () {
-        hulq_landing.init();
-    });
-
-})(jQuery);
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 2 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__app_css__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__app_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__app_css__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__js_main_js__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__js_main_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__js_main_js__);
-// CSS imports
-
-
-// JS imports
-
-
-/***/ }),
-/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -10415,6 +10324,174 @@ if ( !noGlobal ) {
 
 return jQuery;
 } );
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var jQuery = __webpack_require__(0);
+
+(function ($) {
+
+    var landing_cars = {
+
+        elements: {
+            carCentral: $('.car-landing--center')
+        },
+
+        init: function () {
+            var that = this;
+
+            this.animateTopCar();
+
+            $(window).on('scroll.top-car', that.scrollTopCar.bind(this));
+        },
+
+        animateTopCar: function () {
+            var that = this;
+            this.elements.carCentral.addClass('appear');
+            setTimeout(function () {
+                that.elements.carCentral.addClass('appeared');
+            }, 800);
+        },
+
+        scrollTopCar: function () {
+
+            // end target for central car transform = -40%
+            // starting from -80%, finishing after 2x window heights scrolled
+
+            var scrolledHeight = $(document).scrollTop(),
+                doubleWindowHeight = $(window).height() * 2,
+                scrolledPercent = scrolledHeight / doubleWindowHeight * 40,
+                transformValue = scrolledPercent > 40 ? -40 : scrolledPercent - 80;
+
+            this.elements.carCentral.attr('style', 'transform: translate(-50%, ' + transformValue + '%);');
+        }
+    };
+
+    $(function () {
+        landing_cars.init();
+    });
+
+})(jQuery);
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var jQuery = __webpack_require__(0);
+
+(function ($) {
+
+    var landing_text = {
+
+        elements: {
+            contentBlocks: $('.landing-block'),
+            contentBlockFirst: $('.landing-block--top'),
+            contentBlockLast: $('.landing-block--bottom'),
+            landingSectionTop: $('.landing-section__top'),
+            actionButtonMain: $('.button--landing-main')
+        },
+
+        init: function () {
+            var that = this;
+
+            this.showInitialText();
+
+            $(window).on('scroll.animate-text', that.scrolling.bind(this));
+            $(window).on('scroll.text-hide', that.scrollTextHide.bind(this));
+            $(window).on('scroll', that.toggleActionButtonView.bind(this));
+        },
+
+        showInitialText: function () {
+            var that = this;
+            this.elements.contentBlockFirst.add(this.elements.actionButtonMain).addClass('appear');
+
+            setTimeout(function () {
+                that.elements.contentBlockFirst.addClass('appeared');
+            }, 700);
+        },
+
+        scrolling: function () {
+            var documentHeight = $(document).height(),
+                scrolledHeight = $(document).scrollTop(),
+                windowHeight = $(window).height(),
+                combinedHeight = scrolledHeight + windowHeight;
+
+            this.elements.contentBlocks.not('.landing-block--top').each(function () {
+                var $this = $(this);
+                if (scrolledHeight + windowHeight + 50 >= $this.offset().top) {
+                    $this.addClass('appear');
+                }
+            });
+
+            if (!this.elements.contentBlocks.not('.visible').length) {
+                console.log('unboun');
+                $(window).off('scroll.animate-text');
+            }
+        },
+
+        scrollTextHide: function () {
+            if ($(document).scrollTop() === 0) {
+                return;
+            }
+
+            var scrolledHeight = $(document).scrollTop(),
+                operationHeight = this.elements.landingSectionTop.height() / 2,
+                opacityValue;
+
+            if (scrolledHeight <= operationHeight) {
+                opacityValue = (1 - scrolledHeight / operationHeight).toFixed(2);
+                this.elements.contentBlockFirst.attr('style', 'opacity: ' + opacityValue);
+            } else {
+                this.elements.contentBlockFirst.attr('style', 'opacity: ' + 0);
+            }
+
+        },
+
+        toggleActionButtonView: function () {
+            var scrolledHeight = $(document).scrollTop(),
+                windowHeight = $(window).height();
+
+            if (scrolledHeight + windowHeight >= this.elements.contentBlockLast.offset().top) {
+                this.elements.actionButtonMain.addClass('disappear');
+            } else {
+                this.elements.actionButtonMain.removeClass('disappear');
+            }
+
+        }
+    };
+
+    $(function () {
+        landing_text.init();
+    });
+
+})(jQuery);
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__app_css__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__app_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__app_css__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__js_landing_text_js__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__js_landing_text_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__js_landing_text_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__js_landing_cars_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__js_landing_cars_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__js_landing_cars_js__);
+// CSS imports
+
+
+// JS imports
+
 
 
 /***/ })
