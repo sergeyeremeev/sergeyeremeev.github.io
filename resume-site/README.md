7f94e87a7e9a5039dc1c2b17c06627775e60de31
sergggio <13.eremeev.sv@gmail.com>
2019-03-11 17:27:06 +0300

    fixes

--- resume-site/README.md
+++
@@ -1,1 +0,0 @@
-My Resume/Portfolio Website, built with React
--- resume-site/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const rewireStyledComponents = require('react-app-rewire-styled-components');
-
-/* config-overrides.js */
-module.exports = function override(config, env) {
-    /* eslint-disable no-param-reassign */
-    config = rewireStyledComponents(config, env);
-    /* eslint-enable no-param-reassign */
-    return config;
-};
